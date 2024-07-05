1444babce519089fb9e6e8d6ff04acee61881540
illyrius666 <FitimQ@live.nl>
2024-07-05 06:40:28 +0000

    chore: Update typed-animation.mjs with new strings

--- pages/assets/js/typed-animation.mjs
+++ pages/assets/js/typed-animation.mjs
@@ -1,1 +1,1 @@
-new Typed(".auto-type",{strings:["Modular","DRY","Efficient"],typeSpeed:150,backSpeed:100,loop:!0});
+new Typed(".auto-type",{strings:["Modular","Structured","Efficient"],typeSpeed:150,backSpeed:100,loop:!0});
