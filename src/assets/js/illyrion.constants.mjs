8f21082214c2a665af8d09c558a9363130f142ca
illyrius666 <FitimQ@live.nl>
2024-07-11 08:23:42 +0000

    + renamed autotype -> typed to reflect better on what lib is used + renamed a constant + adjusted index to reflect to the renaming

--- src/assets/js/illyrion.constants.mjs
+++ src/assets/js/illyrion.constants.mjs
@@ -1,7 +1,7 @@
 // illyrion.constants.mjs
 
 // AutoType
-export const AT_TARGET = ".autotype";
+export const AT_TARGET = ".typed";
 export const AT_STRINGS = ["MODULAR", "STRUCTURED", "EFFICIENT"];
 export const AT_TYPE_SPEED = 150;
 export const AT_BACK_SPEED = 100;
