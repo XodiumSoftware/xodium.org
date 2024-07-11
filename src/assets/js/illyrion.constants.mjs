41ea41ad42f8befea1a1aef9d63190c4a48c60d6
illyrius666 <FitimQ@live.nl>
2024-07-11 08:05:46 +0000

    - removed codemirror

--- src/assets/js/illyrion.constants.mjs
+++ src/assets/js/illyrion.constants.mjs
@@ -7,16 +7,6 @@ export const AT_TYPE_SPEED = 150;
 export const AT_BACK_SPEED = 100;
 export const AT_LOOP = true;
 
-// CodeMirror
-export const CM_TARGET = "editor";
-export const CM_INDENT_UNIT = 4;
-export const CM_INDENT_WITH_TABS = true;
-export const CM_LINE_NUMBERS = true;
-export const CM_MODE = "python";
-export const CM_THEME = "monokai";
-export const CM_LINE_WRAPPING = true;
-export const CM_READ_ONLY = true;
-
 // ComponentsLoader
 export const COMPONENTS_FOLDER = "../src/assets/components/";
 export const COMPONENTS_MAP = {
