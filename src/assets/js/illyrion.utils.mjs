0931cc92d017e1df2370c216878a2931e7637e48
illyrius666 <FitimQ@live.nl>
2024-07-11 07:43:17 +0000

    + Refactor componentsloader for improved performance and maintainability

--- src/assets/js/illyrion.utils.mjs
+++ src/assets/js/illyrion.utils.mjs
@@ -1,52 +1,98 @@
 // illyrion.utils.mjs
 
-function initializeNavbarBurgers() {
-  const $navbarBurgers = Array.from(
-    document.querySelectorAll(".navbar-burger")
+import { COMPONENTS_MAP } from "./illyrion.constants.mjs";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cache = {};
+
+  async function loadComponent(componentName) {
+    const file = COMPONENTS_MAP[componentName];
+    if (!file) {
+      throw new Error(`Component ${componentName} not found.`);
+    }
+
+    const placeholders = document.querySelectorAll(
+      `[data-component="${componentName}"]`
+    );
+    if (!placeholders.length) {
+      return;
+    }
+
+    let html;
+    if (cache[componentName]) {
+      html =
+        cache[componentName] instanceof Promise
+          ? await cache[componentName]
+          : cache[componentName];
+    } else {
+      cache[componentName] = fetch(file).then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      });
+      html = await cache[componentName];
+    }
+
+    placeholders.forEach((placeholder) => {
+      placeholder.innerHTML = html;
+    });
+  }
+
+  const componentPromises = Object.keys(COMPONENTS_MAP).map((componentName) =>
+    loadComponent(componentName)
   );
 
-  $navbarBurgers.forEach((el) => {
-    if (!el.dataset.initialized) {
-      el.addEventListener("click", () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+  Promise.all(componentPromises).catch((err) =>
+    console.error("An unexpected error occurred while loading components:", err)
+  );
 
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
+  function initializeNavbarBurgers() {
+    const $navbarBurgers = Array.from(
+      document.querySelectorAll(".navbar-burger")
+    );
 
-      el.dataset.initialized = true;
-    }
-  });
-}
+    $navbarBurgers.forEach((el) => {
+      if (!el.dataset.initialized) {
+        el.addEventListener("click", () => {
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
 
-function initializeDropdownChevron() {
-  const toolsDropdown = document.querySelector(".navbar-item.has-dropdown");
-  const chevronIcon = document.getElementById("tools-chevron");
+          el.classList.toggle("is-active");
+          $target.classList.toggle("is-active");
+        });
 
-  if (toolsDropdown && chevronIcon) {
-    if (!toolsDropdown.dataset.initialized) {
-      toolsDropdown.addEventListener("mouseover", () => {
-        chevronIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
-      });
+        el.dataset.initialized = true;
+      }
+    });
+  }
 
-      toolsDropdown.addEventListener("mouseout", () => {
-        chevronIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
-      });
+  function initializeDropdownChevron() {
+    const toolsDropdown = document.querySelector(".navbar-item.has-dropdown");
+    const chevronIcon = document.getElementById("tools-chevron");
 
-      toolsDropdown.dataset.initialized = true;
-    }
-  } else {
-    if (!toolsDropdown) {
-      console.error("toolsDropdown element not found");
-    }
-    if (!chevronIcon) {
-      console.error("chevronIcon element not found");
+    if (toolsDropdown && chevronIcon) {
+      if (!toolsDropdown.dataset.initialized) {
+        toolsDropdown.addEventListener("mouseover", () => {
+          chevronIcon.classList.replace("fa-chevron-down", "fa-chevron-up");
+        });
+
+        toolsDropdown.addEventListener("mouseout", () => {
+          chevronIcon.classList.replace("fa-chevron-up", "fa-chevron-down");
+        });
+
+        toolsDropdown.dataset.initialized = true;
+      }
+    } else {
+      if (!toolsDropdown) {
+        console.error("toolsDropdown element not found");
+      }
+      if (!chevronIcon) {
+        console.error("chevronIcon element not found");
+      }
     }
   }
-}
 
-document.addEventListener("DOMContentLoaded", () => {
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.addedNodes.length) {
