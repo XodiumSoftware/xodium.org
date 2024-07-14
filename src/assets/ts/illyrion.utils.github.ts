43bdea3052262fee4e8b576d5ca6ed16e1308346
Your Name <you@example.com>
2024-07-14 14:04:50 +0000

    + fixed @types minor bugs + added dynamic version fetch + added version into text

--- src/assets/ts/illyrion.utils.github.ts
+++ src/assets/ts/illyrion.utils.github.ts
@@ -1,16 +1,39 @@
-import { GH_ORGNAME } from "./illyrion.constants.js";
+import { GH_ORGNAME, GH_REPONAMES } from "./illyrion.constants.js";
 
 class UtilsGithub {
+  private syncQueue: (() => Promise<void>)[];
+  private readonly syncInterval: number = 60 * 1000;
+
   constructor() {
+    this.syncQueue = [];
+    this.startSyncProcess();
+    this.scheduleSyncOperations();
+
     this.fetchOrgMembers();
+    this.fetchProjectInfo();
   }
 
-  async fetchOrgMembers(): Promise<void> {
-    const oneHour = 60 * 60 * 1000;
-    let members = JSON.parse(localStorage.getItem("members") || "null");
-    let timestamp = Number(localStorage.getItem("timestamp"));
+  private startSyncProcess(): void {
+    setInterval(async () => {
+      if (this.syncQueue.length > 0) {
+        const syncOperation = this.syncQueue.shift();
+        if (syncOperation) {
+          await syncOperation();
+        }
+      }
+    }, this.syncInterval);
+  }
 
-    if (!members || !timestamp || Date.now() - timestamp > oneHour) {
+  private scheduleSyncOperations(): void {
+    this.syncQueue.push(() => this.fetchOrgMembers());
+    this.syncQueue.push(() => this.fetchProjectInfo());
+  }
+
+  private async fetchOrgMembers(): Promise<void> {
+    let membersData = this.getLocalStorageItem("members");
+    let members = membersData ? membersData.value : null;
+
+    if (!members) {
       const response = await fetch(
         `https://api.github.com/orgs/${GH_ORGNAME}/members`,
         {
@@ -20,13 +43,55 @@ class UtilsGithub {
         }
       );
       members = await response.json();
-      localStorage.setItem("members", JSON.stringify(members));
-      localStorage.setItem("timestamp", Date.now().toString());
+      this.setLocalStorageItem("members", members);
     }
+
     this.populateTeamGrid(members);
   }
 
-  populateTeamGrid(members: any[]): void {
+  private async fetchProjectInfo(): Promise<void> {
+    for (const repoName of GH_REPONAMES) {
+      let projectInfoData = this.getLocalStorageItem(
+        `latest_release_${repoName}`
+      );
+      let latestVersion = projectInfoData ? projectInfoData.value : null;
+
+      if (!latestVersion) {
+        const response = await fetch(
+          `https://api.github.com/repos/${GH_ORGNAME}/${repoName}/releases/latest`,
+          {
+            headers: {
+              Accept: "application/vnd.github+json",
+            },
+          }
+        );
+        if (!response.ok) {
+          latestVersion = "n.a.";
+        } else {
+          const latestRelease = await response.json();
+          latestVersion = latestRelease.tag_name;
+        }
+        this.setLocalStorageItem(`latest_release_${repoName}`, latestVersion);
+      }
+
+      this.setVersionInfoText(repoName, latestVersion);
+    }
+  }
+
+  private setLocalStorageItem(key: string, value: any): void {
+    const item = {
+      value: value,
+      timestamp: Date.now(),
+    };
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  private getLocalStorageItem(key: string): any {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
+
+  private populateTeamGrid(members: any[]): void {
     const grid = document.querySelector(".team-grid");
     if (grid) {
       members.forEach((member) => {
@@ -44,6 +109,19 @@ class UtilsGithub {
       console.error('Element with class ".team-grid" not found');
     }
   }
+
+  private setVersionInfoText(repoName: string, version: string): void {
+    const els = document.querySelectorAll(`.${repoName}-version`);
+    if (els) {
+      els.forEach((el) => {
+        el.innerHTML = `<i class="fas fa-solid fa-tag"> ${version}`;
+      });
+    } else {
+      console.error(`Element with class ".${repoName}-version" not found`);
+    }
+  }
 }
 
-new UtilsGithub();
+document.addEventListener("DOMContentLoaded", function () {
+  new UtilsGithub();
+});
