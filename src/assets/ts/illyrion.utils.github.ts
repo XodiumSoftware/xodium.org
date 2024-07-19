67b64ae4569dbda1f3db25be5ec25f268fede3cf
illyrius666 <FitimQ@live.nl>
2024-07-19 09:03:57 +0000

    + closes #12

--- src/assets/ts/illyrion.utils.github.ts
+++ src/assets/ts/illyrion.utils.github.ts
@@ -1,94 +1,82 @@
 import { GH_ORGNAME, GH_REPONAMES } from "./illyrion.constants.js";
 
-class UtilsGithub {
-  private syncQueue: (() => Promise<void>)[];
-  private readonly syncInterval: number = 60 * 1000;
-
-  constructor() {
-    this.syncQueue = [];
-    this.startSyncProcess();
-    this.scheduleSyncOperations();
-
-    this.fetchOrgMembers();
-    this.fetchProjectInfo();
-  }
-
-  private startSyncProcess(): void {
-    setInterval(async () => {
-      if (this.syncQueue.length > 0) {
-        const syncOperation = this.syncQueue.shift();
-        if (syncOperation) {
-          await syncOperation();
-        }
+class GithubAPI {
+  static async fetchOrgMembers() {
+    const response = await fetch(
+      `https://api.github.com/orgs/${GH_ORGNAME}/members`,
+      {
+        headers: {
+          Accept: "application/vnd.github+json",
+        },
       }
-    }, this.syncInterval);
+    );
+    return response.json();
   }
 
-  private scheduleSyncOperations(): void {
-    this.syncQueue.push(() => this.fetchOrgMembers());
-    this.syncQueue.push(() => this.fetchProjectInfo());
-  }
-
-  private async fetchOrgMembers(): Promise<void> {
-    let membersData = this.getLocalStorageItem("members");
-    let members = membersData ? membersData.value : null;
-
-    if (!members) {
-      const response = await fetch(
-        `https://api.github.com/orgs/${GH_ORGNAME}/members`,
-        {
-          headers: {
-            Accept: "application/vnd.github+json",
-          },
-        }
-      );
-      members = await response.json();
-      this.setLocalStorageItem("members", members);
-    }
-
-    this.populateTeamGrid(members);
-  }
-
-  private async fetchProjectInfo(): Promise<void> {
-    for (const repoName of GH_REPONAMES) {
-      let projectInfoData = this.getLocalStorageItem(
-        `latest_release_${repoName}`
-      );
-      let latestVersion = projectInfoData ? projectInfoData.value : null;
-
-      if (!latestVersion) {
-        const response = await fetch(
-          `https://api.github.com/repos/${GH_ORGNAME}/${repoName}/releases/latest`,
-          {
-            headers: {
-              Accept: "application/vnd.github+json",
-            },
-          }
-        );
-        if (!response.ok) {
-          latestVersion = "n.a.";
-        } else {
-          const latestRelease = await response.json();
-          latestVersion = latestRelease.tag_name;
-        }
-        this.setLocalStorageItem(`latest_release_${repoName}`, latestVersion);
+  static async fetchProjectInfo(repoName: string) {
+    const response = await fetch(
+      `https://api.github.com/repos/${GH_ORGNAME}/${repoName}/releases/latest`,
+      {
+        headers: {
+          Accept: "application/vnd.github+json",
+        },
       }
-
-      this.setVersionInfoText(repoName, latestVersion);
-    }
+    );
+    return response.json();
   }
+}
 
-  private setLocalStorageItem(key: string, value: any): void {
+class LocalStorageService {
+  static setItem(key: string, value: any) {
     const item = {
       value: value,
       timestamp: Date.now(),
     };
     localStorage.setItem(key, JSON.stringify(item));
   }
-
-  private getLocalStorageItem(key: string): any {
+  static getItem(key: string) {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : null;
+  }
+}
+
+class UtilsGithub {
+  constructor() {
+    Promise.all([
+      this.fetchAndStoreOrgMembers(),
+      this.fetchAndStoreProjectInfo(),
+    ]).catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  }
+
+  async fetchAndStoreOrgMembers() {
+    let membersData = LocalStorageService.getItem("members");
+    let members = membersData ? membersData.value : null;
+    if (!members) {
+      members = await GithubAPI.fetchOrgMembers();
+      LocalStorageService.setItem("members", members);
+    }
+    this.populateTeamGrid(members);
+  }
+
+  async fetchAndStoreProjectInfo() {
+    for (const repoName of GH_REPONAMES) {
+      let projectInfoData = LocalStorageService.getItem(
+        `latest_release_${repoName}`
+      );
+      let latestVersion = projectInfoData ? projectInfoData.version : null;
+      if (!latestVersion) {
+        const response = await GithubAPI.fetchProjectInfo(repoName);
+        latestVersion =
+          response.message === "Not Found" ? "N.A." : response.tag_name;
+        LocalStorageService.setItem(
+          `latest_release_${repoName}`,
+          latestVersion
+        );
+      }
+      this.setVersionInfoText(repoName, latestVersion);
+    }
   }
 
   private populateTeamGrid(members: any[]): void {
@@ -114,7 +102,7 @@ class UtilsGithub {
     const els = document.querySelectorAll(`.${repoName}-version`);
     if (els) {
       els.forEach((el) => {
-        el.innerHTML = `<i class="fas fa-solid fa-tag"> ${version}`;
+        el.innerHTML = `<i class="fas fa-solid fa-tag"> ${version}</i>`;
       });
     } else {
       console.error(`Element with class ".${repoName}-version" not found`);
