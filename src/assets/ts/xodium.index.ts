9a015e438ba40ad2ad8156d5227a6e8f8a283776
Your Name <you@example.com>
2024-07-27 20:14:40 +0000

    + Update webpack configuration for simplified JavaScript bundling and serve with webpack-dev-server

--- src/assets/ts/xodium.index.ts
+++ src/assets/ts/xodium.index.ts
@@ -1,5 +1,8 @@
 // src/assets/ts/index.ts
 // Internal modules
+// scss
+import "../scss/xodium.custom.scss";
+// ts
 import "./xodium.typed";
 import "./xodium.utils";
 import "./xodium.utils.github";
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/assets/ts/xodium.index.ts",
+  entry: path.resolve(__dirname, "src/assets/ts/xodium.index.ts"),
+  devServer: {
+    static: path.resolve(__dirname, "src"),
+    compress: true,
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "src/assets/js"),
+  },
+  optimization: {
+    runtimeChunk: false,
   },
   resolve: {
     extensions: [".ts", ".js"],
@@ -14,6 +21,11 @@ module.exports = {
       {
         test: /\.ts$/,
         use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
         exclude: /node_modules/,
       },
     ],
