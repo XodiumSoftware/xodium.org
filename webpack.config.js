43aa342a0f763fe7dd140e174fb212c98fa8f97b
illyrius666 <fitimq@live.nl>
2024-09-25 09:14:20 +0000

    changelog: + cleaned up bulma tooltip stuff + refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   entry: path.resolve(__dirname, "src/assets/ts/xodium.index.ts"),
