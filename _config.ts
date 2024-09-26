fe992585e503111887d6625cbc010bb5e352f1cd
illyrius666 <fitimq@live.nl>
2024-09-26 09:47:00 +0000

    chore: + added minifycss func to compress css files

--- _config.ts
+++ _config.ts
@@ -7,4 +7,19 @@ site.copyRemainingFiles((path: string) =>
   path.startsWith("") ? path.toLowerCase() : false
 );
 
+site.loadAssets([".css"]);
+
+function minifyCSS(css: string) {
+  return css
+    .replaceAll("\n", " ")
+    .replaceAll(/\s+/g, " ")
+    .replaceAll(/([:;{])\s/g, "$1");
+}
+
+site.process([".css"], (assets) => {
+  for (const asset of assets) {
+    asset.content = minifyCSS(asset.content as string);
+  }
+});
+
 export default site;
