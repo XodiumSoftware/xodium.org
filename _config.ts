041a4912ca5379d1d7da18bbaa0f527c0659f280
illyrius666 <fitimq@live.nl>
2024-09-26 11:13:49 +0000

    chore: + added some more plugins

--- _config.ts
+++ _config.ts
@@ -4,6 +4,9 @@ import minifyHTML from "lume/plugins/minify_html.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import robots from "lume/plugins/robots.ts";
 import svgo from "lume/plugins/svgo.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
 import sourceMaps from "lume/plugins/source_maps.ts";
 
 const site = lume();
@@ -17,6 +20,9 @@ site.use(minifyHTML(/* Options */));
 site.use(sitemap(/* Options */));
 site.use(robots(/* Options */));
 site.use(svgo(/* Options */));
+site.use(esbuild(/* Options */));
+site.use(tailwindcss(/* Options */));
+site.use(postcss(/* Options */));
 site.use(sourceMaps(/* Options */));
 
 export default site;
---
+++ src/assets/modules/xodium.index.ts
@@ -0,0 +1,6 @@
+// xodium.index.ts
+// Internal modules
+import "../../../src_old/xodium.custom.css";
+import "./xodium.typed";
+import "./xodium.utils";
+import "./xodium.utils.github";
