bed56a65f7fc85064008f749f0ccca8ee34259e1
illyrius666 <fitimq@live.nl>
2024-09-26 09:17:55 +0000

    chore: + fixed src being copied litterly, instead of acknownledging that its the "root"

--- _config.ts
+++ _config.ts
@@ -3,5 +3,8 @@ import lume from "lume/mod.ts";
 const site = lume();
 
 site.ignore("README.md", "LICENSE.md", "CODE_OF_CONDUCT.md");
+site.copyRemainingFiles((path: string) =>
+  path.startsWith("") ? path.toLowerCase() : false
+);
 
 export default site;
