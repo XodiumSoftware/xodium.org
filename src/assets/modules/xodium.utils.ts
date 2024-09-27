f671823f292db1dbc5d6c07249eea8a87bcf1503
illyrius666 <fitimq@live.nl>
2024-09-27 12:32:32 +0000

    chore: + added handlescroll util

--- src/assets/modules/xodium.utils.ts
+++ src/assets/modules/xodium.utils.ts
@@ -1,23 +1,38 @@
 // xodium.utils.ts
+
 class Utils {
+  clickEvent = "click";
+  focusoutEvent = "focusout";
+
   constructor() {
-    ["click", "focusout"].forEach((eType) =>
-      document.addEventListener(eType, (e) =>
-        this.handleToggle(e, "data-toggle")
-      )
+    [this.clickEvent, this.focusoutEvent].forEach((eType) =>
+      document.addEventListener(eType, (e) => {
+        e.preventDefault();
+        this.handleToggle(e, "data-toggle", "hidden");
+      })
     );
+    document.addEventListener(this.clickEvent, (e) => {
+      e.preventDefault();
+      this.handleScroll(e, "data-scroll", "smooth");
+    });
   }
 
-  handleToggle = (e: Event, attr: string) => {
+  handleToggle = (e: Event, attr: string, classtype: string) => {
     const target = (e.target as HTMLElement).getAttribute(attr);
     if (target) {
       const element = document.getElementById(target);
-      if (e.type === "click") {
-        element?.classList.toggle("hidden");
-      } else if (e.type === "focusout") {
-        element?.classList.add("hidden");
+      if (e.type === this.clickEvent) {
+        element?.classList.toggle(classtype);
+      } else if (e.type === this.focusoutEvent) {
+        element?.classList.add(classtype);
       }
     }
+  };
+
+  handleScroll = (e: Event, attr: string, behavior: ScrollBehavior) => {
+    document
+      .getElementById((e.target as HTMLElement).getAttribute(attr)!)
+      ?.scrollIntoView({ behavior });
   };
 }
 
