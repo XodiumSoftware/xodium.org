9400577d46cbcb3a2f25d8632f7ff54bba63742f
illyrius666 <fitimq@live.nl>
2024-09-27 12:12:03 +0000

    chore: + Refactor event handling in xodium.utils.ts

--- src/assets/modules/xodium.utils.ts
+++ src/assets/modules/xodium.utils.ts
@@ -1,16 +1,24 @@
 // xodium.utils.ts
 class Utils {
   constructor() {
-    document.addEventListener("click", (e) =>
-      this.handleToggle(e, "data-toggle")
+    ["click", "focusout"].forEach((eType) =>
+      document.addEventListener(eType, (e) =>
+        this.handleToggle(e, "data-toggle")
+      )
     );
   }
 
-  handleToggle = (e: Event, attr: string) =>
-    (e.target as HTMLElement).getAttribute(attr) &&
-    document
-      .getElementById((e.target as HTMLElement).getAttribute(attr)!)
-      ?.classList.toggle("hidden");
+  handleToggle = (e: Event, attr: string) => {
+    const target = (e.target as HTMLElement).getAttribute(attr);
+    if (target) {
+      const element = document.getElementById(target);
+      if (e.type === "click") {
+        element?.classList.toggle("hidden");
+      } else if (e.type === "focusout") {
+        element?.classList.add("hidden");
+      }
+    }
+  };
 }
 
 new Utils();
