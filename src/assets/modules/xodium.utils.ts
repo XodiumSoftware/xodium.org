8a47c05e09ed8f546d5e9de6274676ec7daebc94
illyrius666 <fitimq@live.nl>
2024-09-27 10:04:53 +0000

    chore: + fix navbar item collapse

--- src/assets/modules/xodium.utils.ts
+++ src/assets/modules/xodium.utils.ts
@@ -1,1 +1,9 @@
 // xodium.utils.ts
+const toolsButton = document.getElementById("tools-button");
+const toolsMenu = document.getElementById("tools-menu");
+
+if (toolsButton && toolsMenu) {
+  toolsButton.addEventListener("click", () => {
+    toolsMenu.classList.toggle("hidden");
+  });
+}
