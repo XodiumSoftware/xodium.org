53dcce94aee1ee0763c5cfccf7ec04e8d35f6f43
illyrius666 <fitimq@live.nl>
2024-10-05 13:11:57 +0000

    +

--- src/assets/modules/xodium.index.ts
+++ src/assets/modules/xodium.index.ts
@@ -2,6 +2,7 @@
 import "xodium/typewriter";
 import { Utils } from "xodium/utils";
 import { CLICK_EVENT, FOCUS_OUT_EVENT } from "xodium/constants";
+import { GithubService } from "xodium/utils/github";
 
 Utils.eventListenerManager([
   {
@@ -13,8 +14,12 @@ Utils.eventListenerManager([
     method: () => Utils.handleElementVisibility("visibility", 100),
   },
 ]);
-Utils.populateTeamCards();
+// Utils.populateTeamCards();
 Utils.replaceContents([
   { source: "xCAD", target: ".xCAD-version" },
   { source: "xLIB", target: ".xLIB-version" },
 ]);
+
+export function getMembers(): Promise<unknown[]> {
+  return GithubService.getData("members");
+}
