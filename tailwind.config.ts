78c4be26c30934d6af6bfedf0e8d0a99f1e814ef
illyrius666 <fitimq@live.nl>
2025-02-27 10:56:34 +0000

    Refactor project structure: move favicon.svg and styles.css to src directory, update tailwind.config.ts content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,5 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["{routes,islands,components}/**/*.{ts,tsx,js,jsx}"],
+  content: ["src/{routes,islands,components}/**/*.{ts,tsx,js,jsx}"],
 } satisfies Config;
