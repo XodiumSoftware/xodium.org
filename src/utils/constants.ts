34a777c7abf599ffa71e1efcd825716aa13ac57a
illyrius666 <fitimq@live.nl>
2025-02-27 15:22:50 +0000

    Remove unused FETCH_DATA_MAP constant from constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,12 +4,3 @@
  */
 
 export const config = { version: "2.0.0" };
-export const FETCH_DATA_MAP: Record<
-  string,
-  { url: string; config?: RequestInit }
-> = {
-  members: {
-    url: "https://api.github.com/orgs/XodiumSoftware/public_members",
-    config: { headers: { Accept: "application/vnd.github+json" } },
-  },
-};
