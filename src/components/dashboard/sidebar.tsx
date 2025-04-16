cf9710507a90237865537c0ccacaa43272b2f635
illyrius666 <fitimq@live.nl>
2025-04-16 15:10:38 +0200

    refactor
    Signed-off-by: illyrius666 <fitimq@live.nl>

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -6,11 +6,11 @@
 import LogOutButton from "../../islands/logout.tsx";
 
 export default function SideBar() {
-    return (
-        <aside
-            className="sidebar fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col border-r border-gray-200 dark:border-gray-800 px-5 transition-all duration-300 lg:static lg:translate-x-0 -translate-x-full">
-            {/* TODO: Add content */}
-            <LogOutButton/>
-        </aside>
-    );
+  return (
+      <aside
+          className="sidebar fixed top-0 left-0 z-9999 flex h-screen w-[290px] flex-col border-r border-gray-200 dark:border-gray-800 px-5 transition-all duration-300 lg:static lg:translate-x-0 -translate-x-full">
+        {/* TODO: Add content */}
+        <LogOutButton/>
+      </aside>
+  );
 }
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -27,7 +27,6 @@ export default async function Dashboard(req: Request) {
         <div className="flex flex-1">
           <SideBar />
           <div className="flex flex-col flex-1">
-            <header></header>
             <div className="flex-grow container mx-auto my-8 px-4 py-8 rounded-xl border-r border-gray-200 dark:border-gray-800">
               {/* TODO: Add content */}
             </div>
