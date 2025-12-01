6a7d8118dc17748bd9fc89b4d5a25ab3762c9893
Illyrius <FitimQ@live.nl>
2025-12-01 13:44:27 +0100

    CSP close #105
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-import {App, staticFiles, trailingSlashes} from "fresh";
+import {App, csp, staticFiles, trailingSlashes} from "fresh";
 import {type State} from "./utils.ts";
 import githubPlugin from "./plugins/github.ts";
 
 export const app = new App<State>();
 
 app.use(staticFiles());
+app.use(csp({
+  reportOnly: false,
+  reportTo: "/api/csp",
+  csp: [
+    "script-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline'",
+    "img-src 'self' data:",
+  ],
+}));
 app.use(trailingSlashes("never"));
 githubPlugin(app);
 app.fsRoutes();
