53c301365f8ba0dbec901bd1cc2b5f097760e197
Illyrius <fitimq@live.nl>
2025-12-16 13:54:15 +0100

    Refactor version display styling in Version component
    Signed-off-by: Illyrius <fitimq@live.nl>

--- components/version.tsx
+++ components/version.tsx
@@ -2,7 +2,7 @@ import {CONFIG} from "../utils.ts";
 
 export default function Version() {
   return (
-    <div className="fixed bottom-0 m-2 text-gray-600 dark:text-slate-400 text-sm">
+    <div className="fixed bottom-0 m-2 text-base-content/60 text-sm">
       v{CONFIG.version}
     </div>
   );
