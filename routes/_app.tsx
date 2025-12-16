d4097fe53aa67e17e71872c51c45b1b6c857506b
Illyrius <fitimq@live.nl>
2025-12-16 14:08:47 +0100

    Update theme to Halloween and enhance DaisyUI plugin configuration
    Signed-off-by: Illyrius <fitimq@live.nl>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,7 +4,7 @@ import {define} from "../utils.ts";
 // noinspection JSUnusedGlobalSymbols
 export default define.page(function App({ Component }) {
   return (
-    <html data-theme="dark">
+    <html data-theme="halloween">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
