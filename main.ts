93af933e0168c64102c2f8f37ffdff8e875abcd1
Illyrius <28700752+illyrius666@users.noreply.github.com>
2025-12-16 11:15:40 +0100

    dev (#112)
    Signed-off-by: Illyrius <fitimq@live.nl>

--- main.ts
+++ main.ts
@@ -1,41 +1,33 @@
-import {App, cors, csp, csrf, staticFiles, trailingSlashes} from "fresh";
+import {App, cors, csrf, staticFiles, trailingSlashes} from "fresh";
 import {type State} from "./utils.ts";
 import githubPlugin from "./plugins/github.ts";
 
 export const app = new App<State>();
 export const allowedOrigins = (origin: string | null): boolean =>
-  origin === "https://xodium.org" || origin?.endsWith(".xodium.org") === true;
+    origin === "https://xodium.org" || origin?.endsWith(".xodium.org") === true;
 
 app.use(staticFiles());
 app.use(
-  cors({
-    origin: (origin) => {
-      if (!origin) return null;
-      if (origin === "https://xodium.org" || /\.xodium\.org$/.test(origin)) {
-        return origin;
-      }
-      return null;
-    },
-    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
-    allowMethods: ["GET", "POST", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    maxAge: 600,
-    credentials: true,
-  }),
+    cors({
+        origin: (origin) => {
+            if (!origin) return null;
+            if (origin === "https://xodium.org" || /\.xodium\.org$/.test(origin)) {
+                return origin;
+            }
+            return null;
+        },
+        allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+        allowMethods: ["GET", "POST", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
 );
 app.use(
-  csrf({
-    origin: allowedOrigins,
-  }),
+    csrf({
+        origin: allowedOrigins,
+    }),
 );
-app.use(csp({
-  reportOnly: false,
-  reportTo: "/api/csp",
-  csp: [
-    "script-src 'self' 'unsafe-inline'",
-    "style-src 'self' 'unsafe-inline'",
-  ],
-}));
 app.use(trailingSlashes("never"));
 githubPlugin(app);
 app.fsRoutes();
