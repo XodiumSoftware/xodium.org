e3b754cf61e1feca14bd4d7bdf8ba47f6ae06a05
Illyrius <FitimQ@live.nl>
2025-12-19 11:36:09 +0100

    Update favicon to include .ico version and bump version to 3.3.1
    Signed-off-by: Illyrius <FitimQ@live.nl>

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -10,6 +10,7 @@ export default define.page(function App({ Component }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Xodium</title>
         <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="bg-base-100 font-mono">
         <Component />
