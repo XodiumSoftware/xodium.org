deadca660f5c2c33b9f7f78c1b5739c564879c08
Illyrius <fitimq@live.nl>
2026-01-29 09:01:50 +0100

    Add Cloudflare Vite plugin and wrangler to project configuration
    Signed-off-by: Illyrius <fitimq@live.nl>

---
+++ server.js
@@ -0,0 +1,6 @@
+import server from "./_fresh/server.js";
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    fetch: server.fetch,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from "vite";
 import {fresh} from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
+import {cloudflare} from "npm:@cloudflare/vite-plugin@1.22.0"; // noinspection JSUnusedGlobalSymbols
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-    plugins: [fresh(), tailwindcss()],
+    plugins: [fresh(), tailwindcss(), cloudflare()],
 });
