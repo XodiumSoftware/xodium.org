8ffd25eddc2787e0f2420205c7136c720d2558f2
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-03-05 15:02:55 +0100

    +
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod app;
+
+use app::App;
 use leptos::prelude::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App);
 }
