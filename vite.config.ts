16797c8535f4c0ab432098a07ea6d1db7124d5b1
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-03-05 08:34:54 +0100

    init
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from "vite";
-import {fresh} from "@fresh/plugin-vite";
-import tailwindcss from "@tailwindcss/vite";
-import {cloudflare} from "npm:@cloudflare/vite-plugin@1.22.0"; // noinspection JSUnusedGlobalSymbols
-
-// noinspection JSUnusedGlobalSymbols
-export default defineConfig({
-    plugins: [fresh(), tailwindcss(), cloudflare()],
-});
