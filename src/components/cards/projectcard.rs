1d264a77dce4468a0bea432ab1fa838a6749a056
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-06-25 08:28:05 +0200

    Extract inline SVGs into reusable icon files

--- src/components/cards/projectcard.rs
+++ src/components/cards/projectcard.rs
@@ -43,39 +43,26 @@ fn LanguageCircle(language: String) -> impl IntoView {
 #[component]
 fn StarIcon() -> impl IntoView {
     view! {
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
+        <img
+            src="/icons/star.svg"
+            alt=""
             width="14"
             height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-        </svg>
+            class="w-[14px] h-[14px]"
+        />
     }
 }
 
 #[component]
 fn DocsIcon() -> impl IntoView {
     view! {
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
+        <img
+            src="/icons/docs.svg"
+            alt=""
             width="14"
             height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
-            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
-        </svg>
+            class="w-[14px] h-[14px]"
+        />
     }
 }
 
