27c6ed209fa0070723e794cfe6cfa43cd9b2422e
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-06-25 08:30:57 +0200

    revert

--- src/components/cards/projectcard.rs
+++ src/components/cards/projectcard.rs
@@ -42,26 +42,39 @@ fn LanguageCircle(language: String, color: &'static str) -> impl IntoView {
 #[component]
 fn StarIcon() -> impl IntoView {
     view! {
-        <img
-            src="/icons/star.svg"
-            alt=""
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
             width="14"
             height="14"
-            class="w-[14px] h-[14px]"
-        />
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+        </svg>
     }
 }
 
 #[component]
 fn DocsIcon() -> impl IntoView {
     view! {
-        <img
-            src="/icons/docs.svg"
-            alt=""
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
             width="14"
             height="14"
-            class="w-[14px] h-[14px]"
-        />
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
+            <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" />
+        </svg>
     }
 }
 
